64d5593ebd8d2542c6922ce431de800a4ac4df08
Christopher Shimmin-Vincent <cshimminvincent@gmail.com>
2024-02-06 13:46:38 +0000

    Create unit tests

--- todo/test_models.py
+++ todo/test_models.py
@@ -1,8 +1,17 @@
 from django.test import TestCase
+from .models import Item
 
-# Create your tests here.
 
+class TestModels(TestCase):
 
-class TestDjango(TestCase):
-    def test_this_thing_works(self):
-        self.assertEqual(1, 0)
+    def test_done_defaults_to_false(self):
+        # Create a test item
+        item = Item.objects.create(name="Test Todo Item")
+        # Check that done is set to false by default
+        self.assertFalse(item.done)
+
+    def test_item_string_method_returns_name(self):
+        # Create a a test item
+        item = Item.objects.create(name="Test Todo Item")
+        # Check that the string item returns the name
+        self.assertEqual(str(item), 'Test Todo Item')
--- todo/test_views.py
+++ todo/test_views.py
@@ -1,8 +1,75 @@
 from django.test import TestCase
+# Import the Item model from models.py so we can test dabatase functionality
+from .models import Item
 
-# Create your tests here.
 
+class TestViews(TestCase):
 
-class TestDjango(TestCase):
-    def test_this_thing_works(self):
-        self.assertEqual(1, 0)
+    def test_get_todo_list(self):
+        # Get the URL from Django URLs
+        response = self.client.get('/')
+        # Check the status code is OK (i.e. OK)
+        self.assertEqual(response.status_code, 200)
+        # Check the page template displayed is correct
+        self.assertTemplateUsed(response, 'todo/todo_list.html')
+
+    def test_get_add_item_page(self):
+        response = self.client.get('/add')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/add_item.html')
+
+    def test_get_edit_item_page(self):
+        # Create an item to test the response
+        item = Item.objects.create(name="Test Todo Item")
+        # Use string literal to get the correct URL based on the item.id
+        # created in the database
+        response = self.client.get(f'/edit/{item.id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/edit_item.html')
+
+    def test_can_add_item(self):
+        # Simulate submitting the Add Item form with data
+        response = self.client.post('/add', {'name': 'Test Added Item'})
+        # Check that the Add Item page redirects to homepage when
+        # form is subbmited via POST method
+        self.assertRedirects(response, '/')
+
+    def test_can_delete_item(self):
+        # Create an item to be deleted
+        item = Item.objects.create(name="Test Todo Item")
+        # Use string literal to get the correct URL based on the item.id
+        # created in the database
+        response = self.client.get(f'/delete/{item.id}')
+        # Check that the Delete Item page redirects to homepage when
+        # form is subbmited via POST method
+        self.assertRedirects(response, '/')
+        # Check item deleted from database by getting the item ID
+        # then checking the length to confirm it is 0 (i.e. deleted)
+        existing_items = Item.objects.filter(id=item.id)
+        self.assertEqual(len(existing_items), 0)
+
+    def test_can_toggle_item(self):
+        # Create an item to be toggled
+        item = Item.objects.create(name="Test Todo Item", done=True)
+        # Use string literal to get the correct URL based on the item.id
+        # created in the database
+        response = self.client.get(f'/toggle/{item.id}')
+        # Check that the Toggle Item page redirects to homepage when
+        # form is subbmited via POST method
+        self.assertRedirects(response, '/')
+        # Check the item's done status has been updated to False
+        updated_item = Item.objects.get(id=item.id)
+        self.assertFalse(updated_item.done)
+
+    def test_can_edit_item(self):
+        # Create an item to test the response
+        item = Item.objects.create(name="Test Todo Item")
+        # Use string literal to get the correct URL based on the item.id
+        # created in the database and update the name to test edit works
+        response = self.client.post(f'/edit/{item.id}',
+                                    {'name': 'Updated Name'})
+        # Test application reqdirects to homepage once edited
+        self.assertRedirects(response, '/')
+        # Check item updated in database
+        updated_item = Item.objects.get(id=item.id)
+        self.assertEqual(updated_item.name, 'Updated Name')
